7f3a44b34f74a490cc320bd670f6e5b97592b7b7
noah0316 <noa316@naver.com>
2020-11-17 12:27:12 +0900

    Learned how to use for loop in Swift

--- playground/Flow_Control.playground/Contents.swift
+++ playground/Flow_Control.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 
 // ---- While
 
@@ -27,3 +28,52 @@ repeat {
 //
 //repeat while
 // Code execution > Condition check > Code execution > Condition check
+
+
+
+// ---- For Loop
+
+let closedRange = 0...10 // 0~10
+let halfClosedRange = 0..<10 // 0~9
+
+var sum = 0
+for i in halfClosedRange {
+    print("---> \(i)")
+    sum += i
+}
+
+print("---> total sum: \(sum)")
+
+var sinValue: CGFloat = 0
+for i in closedRange{
+    sinValue = sin(CGFloat.pi/4 * CGFloat(i))
+}
+
+let name = "Noah"
+for _ in closedRange {
+    print("---> name: \(name)")
+}
+
+for i in closedRange {
+    if i % 2 == 0 {
+        print("---> 짝수: \(i)")
+    }
+}
+
+for i in closedRange where i % 2 == 0 {
+    print("---> 짝수: \(i)")
+}
+
+for i in closedRange {
+    if i == 3 {
+        continue
+    }
+    
+    print("---> \(i)")
+}
+
+for i in closedRange {
+    for j in closedRange {
+        print("gugu -> \(i) * \(j) = \(i*j)")
+    }
+}
