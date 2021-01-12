b660a73c288ccbdd32966e735dfcfe314ba7c5fb
Noah <noa316@naver.com>
2021-01-12 22:10:08 +0900

    Learned how to use json type in Swift

--- TodoList/TodoList/Todo.swift
+++ TodoList/TodoList/Todo.swift
@@ -4,6 +4,7 @@
 //
 //  Created by joonwon lee on 2020/03/19.
 //  Copyright © 2020 com.joonwon. All rights reserved.
+//  Modified by Noah on 2021/01/12.
 //
 
 import UIKit
@@ -15,63 +16,72 @@ struct Todo: Codable, Equatable {
     var isDone: Bool
     var detail: String
     var isToday: Bool
-    
+
     mutating func update(isDone: Bool, detail: String, isToday: Bool) {
-        // TODO: update 로직 추가
-        
+        // update 로직
+        self.isDone = isDone
+        self.detail = detail
+        self.isToday = isToday
     }
-    
+
     static func == (lhs: Self, rhs: Self) -> Bool {
-        // TODO: 동등 조건 추가
-        return true
+        // 동등 조건 추가
+        return lhs.id == rhs.id
     }
 }
 
 class TodoManager {
-    
+
     static let shared = TodoManager()
-    
+
     static var lastId: Int = 0
-    
+
     var todos: [Todo] = []
-    
+
     func createTodo(detail: String, isToday: Bool) -> Todo {
-        //TODO: create로직 추가
-        return Todo(id: 1, isDone: false, detail: "2", isToday: true)
+        // create로직
+        let nextId = TodoManager.lastId + 1
+        TodoManager.lastId = nextId
+        return Todo(id: nextId, isDone: false, detail: detail, isToday: isToday)
     }
-    
+
     func addTodo(_ todo: Todo) {
-        //TODO: add로직 추가
+        // add로직
+        todos.append(todo)
+        saveTodo()
     }
-    
+
     func deleteTodo(_ todo: Todo) {
-        //TODO: delete 로직 추가
-        
+        // delete 로직
+        todos = todos.filter { $0.id != todo.id }
+        saveTodo()
     }
-    
+
     func updateTodo(_ todo: Todo) {
-        //TODO: updatee 로직 추가
-        
+        // update 로직
+        guard let index = todos.firstIndex(of: todo) else { return }
+        todos[index].update(isDone: todo.isDone, detail: todo.detail, isToday: todo.isToday)
+        saveTodo()
     }
-    
+
     func saveTodo() {
         Storage.store(todos, to: .documents, as: "todos.json")
     }
-    
+
     func retrieveTodo() {
         todos = Storage.retrive("todos.json", from: .documents, as: [Todo].self) ?? []
-        
+
         let lastId = todos.last?.id ?? 0
         TodoManager.lastId = lastId
     }
 }
 
 class TodoViewModel {
-    
+
     enum Section: Int, CaseIterable {
         case today
         case upcoming
-        
+
         var title: String {
             switch self {
             case .today: return "Today"
@@ -79,37 +89,37 @@ class TodoViewModel {
             }
         }
     }
-    
+
     private let manager = TodoManager.shared
-    
+
     var todos: [Todo] {
         return manager.todos
     }
-    
+
     var todayTodos: [Todo] {
         return todos.filter { $0.isToday == true }
     }
-    
+
     var upcompingTodos: [Todo] {
         return todos.filter { $0.isToday == false }
     }
-    
+
     var numOfSection: Int {
         return Section.allCases.count
     }
-    
+
     func addTodo(_ todo: Todo) {
         manager.addTodo(todo)
     }
-    
+
     func deleteTodo(_ todo: Todo) {
         manager.deleteTodo(todo)
     }
-    
+
     func updateTodo(_ todo: Todo) {
         manager.updateTodo(todo)
     }
-    
+
     func loadTasks() {
         manager.retrieveTodo()
     }
--- TodoList/TodoList/TodoListViewController.swift
+++ TodoList/TodoList/TodoListViewController.swift
@@ -4,44 +4,57 @@
 //
 //  Created by joonwon lee on 2020/03/19.
 //  Copyright © 2020 com.joonwon. All rights reserved.
+//  Modified by Noah on 2021/01/12.
 //
 
 import UIKit
 
 class TodoListViewController: UIViewController {
-    
+
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var inputViewBottom: NSLayoutConstraint!
     @IBOutlet weak var inputTextField: UITextField!
-    
+
     @IBOutlet weak var isTodayButton: UIButton!
     @IBOutlet weak var addButton: UIButton!
-    
-    
-    // TODO: TodoViewModel 만들기
-    
-    
+
+
+    // TodoViewModel
+    let todoListViewModel = TodoViewModel()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // TODO: 키보드 디텍션
+
+
+        // 데이터 불러오기
+        todoListViewModel.loadTasks()
         
-        
-        // TODO: 데이터 불러오기
-        
+//        let todo = TodoManager.shared.createTodo(detail: "👍 🚀 Corona 난리", isToday: true)
+//        Storage.saveTodo(todo, fileName: "test.json")
+
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        let todo = Storage.restoreTodo("test.json")
+//        print("---> restore from disk: \(todo)")
+    }
+
     @IBAction func isTodayButtonTapped(_ sender: Any) {
-        // TODO: 투데이 버튼 토글 작업
-        
+        // 투데이 버튼 토글
+        isTodayButton.isSelected = !isTodayButton.isSelected
     }
-    
+
     @IBAction func addTaskButtonTapped(_ sender: Any) {
         // TODO: Todo 태스크 추가
         // add task to view model
         // and tableview reload or update
     }
-    
+
     // TODO: BG 탭했을때, 키보드 내려오게 하기
 }
 
@@ -49,45 +62,54 @@ extension TodoListViewController {
     @objc private func adjustInputView(noti: Notification) {
         guard let userInfo = noti.userInfo else { return }
         // TODO: 키보드 높이에 따른 인풋뷰 위치 변경
-        
+
     }
 }
 
 extension TodoListViewController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // TODO: 섹션 몇개
-        return 0
+        // 섹션 몇개인지
+        return todoListViewModel.numOfSection
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // TODO: 섹션별 아이템 몇개
-        return 0
+        // 섹션별 아이템 몇개인지
+        if section == 0 {
+            return todoListViewModel.todayTodos.count
+        } else {
+            return todoListViewModel.upcompingTodos.count
+        }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        // TODO: 커스텀 셀
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TodoListCell", for: indexPath) as? TodoListCell else {
             return UICollectionViewCell()
         }
-        return cell
-        
-        // TODO: todo 를 이용해서 updateUI
+
+        var todo: Todo
+        if indexPath.section == 0 {
+            todo = todoListViewModel.todayTodos[indexPath.item]
+        } else {
+            todo = todoListViewModel.upcompingTodos[indexPath.item]
+        }
+        cell.updateUI(todo: todo)
+
         // TODO: doneButtonHandler 작성
         // TODO: deleteButtonHandler 작성
         return cell
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
         case UICollectionView.elementKindSectionHeader:
             guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "TodoListHeaderView", for: indexPath) as? TodoListHeaderView else {
                 return UICollectionReusableView()
             }
-            
+
             guard let section = TodoViewModel.Section(rawValue: indexPath.section) else {
                 return UICollectionReusableView()
             }
-            
+
             header.sectionTitleLabel.text = section.title
             return header
         default:
@@ -98,42 +120,44 @@ extension TodoListViewController: UICollectionViewDataSource {
 
 extension TodoListViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        // TODO: 사이즈 계산하기
-        return CGSize.zero
+        // 사이즈 계산하기
+        let width: CGFloat = collectionView.bounds.width
+        let height: CGFloat = 50
+        return CGSize(width: width, height: height)
     }
 }
 
 class TodoListCell: UICollectionViewCell {
-    
+
     @IBOutlet weak var checkButton: UIButton!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var deleteButton: UIButton!
     @IBOutlet weak var strikeThroughView: UIView!
-    
+
     @IBOutlet weak var strikeThroughWidth: NSLayoutConstraint!
-    
+
     var doneButtonTapHandler: ((Bool) -> Void)?
     var deleteButtonTapHandler: (() -> Void)?
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         reset()
     }
-    
+
     override func prepareForReuse() {
         super.prepareForReuse()
         reset()
     }
-    
+
     func updateUI(todo: Todo) {
-        // TODO: 셀 업데이트 하기
+        // 셀 업데이트
         checkButton.isSelected = todo.isDone
         descriptionLabel.text = todo.detail
         descriptionLabel.alpha = todo.isDone ? 0.2 : 1
         deleteButton.isHidden = todo.isDone == false
         showStrikeThrough(todo.isDone)
     }
-    
+
     private func showStrikeThrough(_ show: Bool) {
         if show {
             strikeThroughWidth.constant = descriptionLabel.bounds.width
@@ -141,28 +165,35 @@ class TodoListCell: UICollectionViewCell {
             strikeThroughWidth.constant = 0
         }
     }
-    
-    func reset() {
-        // TODO: reset로직 구현
-        
-    }
-    
-    @IBAction func checkButtonTapped(_ sender: Any) {
-        // TODO: checkButton 처리
-        
 
+    func reset() {
+        // reset로직
+        descriptionLabel.alpha = 1
+        deleteButton.isHidden = true
+        showStrikeThrough(false)
     }
-    
+
+    @IBAction func checkButtonTapped(_ sender: Any) {
+        // checkButton 처리
+        checkButton.isSelected = !checkButton.isSelected
+        let isDone = checkButton.isSelected
+        showStrikeThrough(isDone)
+        descriptionLabel.alpha = isDone ? 0.2 : 1
+        deleteButton.isHidden = !isDone
+        
+        doneButtonTapHandler?(isDone)
+    }
+
     @IBAction func deleteButtonTapped(_ sender: Any) {
-        // TODO: deleteButton 처리 
+        // deleteButton 처리
         deleteButtonTapHandler?()
     }
 }
 
 class TodoListHeaderView: UICollectionReusableView {
-    
+
     @IBOutlet weak var sectionTitleLabel: UILabel!
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
     }
