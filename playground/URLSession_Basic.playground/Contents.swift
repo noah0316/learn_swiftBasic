7c9b6f2d562bc8d04e941c1a98cb6723de461a1a
Noah <noa316@naver.com>
2021-01-21 00:11:51 +0900

    Learned how to send request and receive response data using URLSession

--- playground/URLSession_Basic.playground/Contents.swift
+++ playground/URLSession_Basic.playground/Contents.swift
@@ -26,15 +26,60 @@ relativeURL?.baseURL
 
 // URLComponents
 
-var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")
+//var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")
+//let mediaQuery = URLQueryItem(name: "media", value: "music")
+//let entityQuery = URLQueryItem(name: "entity", value: "song")
+//let termQuery = URLQueryItem(name: "term", value: "지드래곤")
+//
+//urlComponents?.queryItems?.append(mediaQuery)
+//urlComponents?.queryItems?.append(entityQuery)
+//urlComponents?.queryItems?.append(termQuery)
+//
+//urlComponents?.url
+//urlComponents?.string
+//urlComponents?.queryItems
+
+
+// URLSession
+
+// 1. URLSessionConfiguration
+// 2. URLSession
+// 3. URLSessionTask 를 이용해서 서버와 네트워킹
+
+// URLSessionTask
+
+// - dataTask
+// - uploadTask
+// - downloadTask
+
+let config = URLSessionConfiguration.default
+let session = URLSession(configuration: config)
+
+// URL
+// URLComponents
+var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
 let mediaQuery = URLQueryItem(name: "media", value: "music")
 let entityQuery = URLQueryItem(name: "entity", value: "song")
 let termQuery = URLQueryItem(name: "term", value: "지드래곤")
+urlComponents.queryItems?.append(mediaQuery)
+urlComponents.queryItems?.append(entityQuery)
+urlComponents.queryItems?.append(termQuery)
+let requestURL = urlComponents.url!
 
-urlComponents?.queryItems?.append(mediaQuery)
-urlComponents?.queryItems?.append(entityQuery)
-urlComponents?.queryItems?.append(termQuery)
+let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
+    guard error == nil else { return }
+    
+    guard let statusCode = (response as? HTTPURLResponse)?.statusCode else { return }
+    let successRange = 200..<300
+    
+    guard successRange.contains(statusCode) else {
+        // handle response error
+        return
+    }
+    
+    guard let resultData = data else { return }
+    let resultString = String(data: resultData, encoding: .utf8)
+    print("---> result: \(resultString)")
+}
 
-urlComponents?.url
-urlComponents?.string
-urlComponents?.queryItems
+dataTask.resume()
