29500b7dcc1608e831ca6713179c6fd41c98a06d
Noah <noa316@naver.com>
2021-01-22 23:52:56 +0900

    Learned how to parsing response data using codable

--- playground/URLSession_Basic.playground/Contents.swift
+++ playground/URLSession_Basic.playground/Contents.swift
@@ -66,6 +66,29 @@ urlComponents.queryItems?.append(entityQuery)
 urlComponents.queryItems?.append(termQuery)
 let requestURL = urlComponents.url!
 
+struct Response: Codable {
+    let resultCount: Int
+    let tracks: [Track]
+    
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case tracks = "results"
+    }
+}
+
+struct Track: Codable {
+    let title: String
+    let artistName: String
+    let thumbnailPath: String
+        
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case artistName
+        case thumbnailPath = "artworkUrl100"
+    }
+}
+
+
 let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
     guard error == nil else { return }
     
@@ -79,7 +102,35 @@ let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
     
     guard let resultData = data else { return }
     let resultString = String(data: resultData, encoding: .utf8)
-    print("---> result: \(resultString)")
+    
+    // 목표: 트랙리스트 object로 가져오기
+    
+    // TODO:
+    // - Data -> Track목록으로 가져오기[Track]
+    // - Track 오브젝트를 만들어야겠다.
+    // - Data에서 struct로 파싱하고 싶다. > Codable 이용해서 만들자
+    //  - Json 파일, 데이터 > 오브젝트 (Codable 이용)
+    //  - Response, Track 두개 만들예정
+    
+    
+    // 해야할 일
+    // - Response, Track, struct
+    // - struct의 프로퍼티 이름과 실제 데이터의 키와 맞추기 (Codable 디코딩하기 위해서)
+    // - Decoding(파싱하기)
+    // - 트랙리스트 가져오기
+    
+    
+    // 파싱 및 트랙 가져오기
+    do {
+        let decoder = JSONDecoder()
+        let response = try decoder.decode(Response.self, from: resultData)
+        let tracks = response.tracks
+        
+        print("---> tracks: \(tracks.count) -\(tracks.first?.title), \(tracks.first?.thumbnailPath)")
+    } catch let error {
+        print("---> error: \(error.localizedDescription)")
+    }
+//    print("---> result: \(resultString)")
 }
 
 dataTask.resume()
