fa702a3ae8a54f7a6f03f74b055b8939c0ab8ff8
Noah <noa316@naver.com>
2021-01-25 22:48:03 +0900

    Implement Search Bar

--- MyNetflix/MyNetflix/SearchViewController.swift
+++ MyNetflix/MyNetflix/SearchViewController.swift
@@ -4,14 +4,34 @@
 //
 //  Created by joonwon lee on 2020/04/02.
 //  Copyright © 2020 com.joonwon. All rights reserved.
+//  Modified by Noah on 2021/01/25.
 //
 
 import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    private func dismissKeyboard() {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        dismissKeyboard()
+        guard let searchTerm = searchBar.text,
+              searchTerm.isEmpty == false else { return }
+        
+        print("---> 검색어: \(searchTerm)")
+    }
 }
