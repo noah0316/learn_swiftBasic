041987ff3cb6752ebd193b069c7e0e1711368bae
Noah <noa316@naver.com>
2021-01-31 02:25:16 +0900

    Parse response data using Codable

--- MyNetflix/MyNetflix/SearchViewController.swift
+++ MyNetflix/MyNetflix/SearchViewController.swift
@@ -13,34 +13,35 @@ class SearchViewController: UIViewController {
 
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var resultCollectionView: UICollectionView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
 }
 
 extension SearchViewController: UISearchBarDelegate {
-    
+
     private func dismissKeyboard() {
         searchBar.resignFirstResponder()
     }
-    
+
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
+
         dismissKeyboard()
         guard let searchTerm = searchBar.text,
-              searchTerm.isEmpty == false else { return }
-        // 네트워킹을 통한 검색
+            searchTerm.isEmpty == false else { return }
+        // TODO: 네트워킹을 통한 검색
         // - 목표: search term을 가지고 네트워킹을 통해서 영화검색
         // - 검색 API가 필요
-        // - 결과를 받아올 모델 Movie,Response
+        // - 결과를 받아올 모델 Movie, Response
         // - 결과를 받아와서, CollectionView로 표현
-        
+
         SearchAPI.search(searchTerm) { movies in
             // collectionView로 표현하기
+            print(movies.count, movies.first?.title)
         }
-        
+
         print("---> 검색어: \(searchTerm)")
     }
 }
@@ -48,7 +49,7 @@ extension SearchViewController: UISearchBarDelegate {
 class SearchAPI {
     static func search(_ term: String, completion: @escaping ([Movie]) -> Void) {
         let session = URLSession(configuration: .default)
-        
+
         var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
         let mediaQuery = URLQueryItem(name: "media", value: "movie")
         let entityQuery = URLQueryItem(name: "entity", value: "movie")
@@ -56,40 +57,70 @@ class SearchAPI {
         urlComponents.queryItems?.append(mediaQuery)
         urlComponents.queryItems?.append(entityQuery)
         urlComponents.queryItems?.append(termQuery)
-        
+
         let requestURL = urlComponents.url!
-        
+
         let dataTask = session.dataTask(with: requestURL) { data, response, error in
             let successRange = 200..<300
-            
+
             guard error == nil,
-                  let statusCode = (response as? HTTPURLResponse)?.statusCode,
-                  successRange.contains(statusCode) else {
-                    completion([])
-                    return
-                
+                let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                successRange.contains(statusCode) else {
+                completion([])
+                return
+
             }
-            
+
             guard let resultData = data else {
                 completion([])
                 return
             }
-            
+
             // data -> [Movie]
-            let string = String(data: resultData, encoding: .utf8)
-            print("---> result:\(string)")
-//            completion([Movie])
+            let movies = SearchAPI.parseMovies(resultData)
+            completion(movies)
         }
         dataTask.resume()
-            
-        }
+
     }
 
+    static func parseMovies(_ data: Data) -> [Movie] {
+        let decoder = JSONDecoder()
 
-struct Response {
-    
+        do {
+            let response = try decoder.decode(Response.self, from: data)
+            let movies = response.movies
+            return movies
+
+
+        } catch let error {
+            print("---> parsing error: \(error.localizedDescription)")
+            return []
+        }
+    }
 }
 
-struct Movie {
-    
+
+struct Response: Codable {
+    let resultCount: Int
+    let movies: [Movie]
+
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case movies = "results"
+    }
+}
+
+struct Movie: Codable {
+    let title: String
+    let director: String
+    let thumbnailPath: String
+    let previewURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case director = "artistName"
+        case thumbnailPath = "artworkUrl100"
+        case previewURL = "previewUrl"
+    }
 }
